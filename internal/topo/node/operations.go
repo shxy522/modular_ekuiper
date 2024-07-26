0d09da85c585ebd9d0bf67a40fea19bea8437057
Song gao <disxiaofei@163.com>
2024-07-26 15:14:50 +0800

    instance id with root
    Signed-off-by: Song gao <disxiaofei@163.com>

--- internal/topo/node/operations.go
+++ internal/topo/node/operations.go
@@ -16,6 +16,7 @@ package node
 
 import (
 	"fmt"
+	"math/rand/v2"
 	"sync"
 
 	"github.com/lf-edge/ekuiper/internal/topo/node/metric"
@@ -81,9 +82,10 @@ func (o *UnaryOperator) Exec(ctx api.StreamContext, errCh chan<- error) {
 	}
 	// reset status
 	o.statManagers = nil
+	root := rand.IntN(100)
 
 	for i := 0; i < o.concurrency; i++ { // workers
-		instance := i
+		instance := i + root
 		go func() {
 			err := infra.SafeRun(func() error {
 				o.doOp(ctx.WithInstance(instance), errCh)
